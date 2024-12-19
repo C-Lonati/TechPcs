a1f6fa370bc353879b2f6727b0ea6dd3c1ad5cf5
C-Lonati <70873223+C-Lonati@users.noreply.github.com>
2024-12-19 14:28:07 +0900

    Add files via upload

---
+++ js/embed.js
@@ -0,0 +1,4 @@
+$(document).ready(function(){
+    $('#header').load('https://c-lonati.github.io/TechPcs/header.html');
+    $('#footer').load('https://c-lonati.github.io/TechPcs/footer.html');
+});
