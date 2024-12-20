9918abc3a1a0265321a2f3f5f880162c175714e9
C-Lonati <70873223+C-Lonati@users.noreply.github.com>
2024-12-20 18:11:09 +0900

    Add files via upload

--- js/embed.js
+++ js/embed.js
@@ -1,19 +1,63 @@
 $(document).ready(function(){
     $('#header').load('https://c-lonati.github.io/TechPcs/header.html');
     $('#footer').load('https://c-lonati.github.io/TechPcs/footer.html');
-    $lang = $('<div class="langBox"><ul><li><img src="images/lang1.svg" alt="한국어">한국어</img></li><li><img src="images/lang2.svg" alt="중국어">中文</img></li><li><img src="images/lang3.svg" alt="일본어">日語</img></li><li><img src="images/lang4.svg" alt="영어(미국)">English(US)</img></li><li><img src="images/lang5.svg" alt="영어(영국)">English(UK)</img></li><li><img src="images/lang6.svg" alt="프랑스어">French</img></li><li><img src="images/lang7.svg" alt="스페인어">Castellano</img></li><li><img src="images/lang8.svg" alt="힌디어">Hindi</img></li></ul></div>');
-    $(document).on('click', $('.lang1>img') ,function(){
-        $lang.prependTo($('#header'));
-        $lang.css('top', '80px');
+    $('#login').load('https://c-lonati.github.io/TechPcs/login.html');
+    $('.langBox').load('https://c-lonati.github.io/TechPcs/lang.html');
+$search = $('<div id="searchBox"><form method="post"><p class="underLine"><input type="search"><input type="image" src="images/search.svg" alt="찾아보기"></p></form></div>');
+$modal = $('<div id="modal"></div>');
+    let searchToggle='off';
+    $('#header>.logo').ready(function(){
+        $('#headLang').on('click',function(){
+            $('.langBox').removeClass('lbFoot hidden');
+            if($('.langBox').hasClass('lbHead')){
+                $('.langBox').removeClass('lbHead');
+                $('.langBox').addClass('hidden');
+            }else $('.langBox').addClass('lbHead');
+        });
+        $('.login').on('click' ,function(){
+            $modal.prependTo($('body'));
+            $('#login').removeClass('hidden');
+        });
+        $('.search').on('click' ,function(){
+            if(searchToggle=='off'){
+                $search.prependTo($('#header'));
+                searchToggle='on';
+            }
+            else{
+                $search.detach();
+                searchToggle='off';
+            } 
+        });
+        $('.langBox img').on('click', function(){
+            alert('클릭');
+        });
     });
-    $(document).on('click', $('.lang2>img') ,function(){
-        $lang.prependTo($('#footer'));
-        $lang.css('bottom', '100px');
+    $modal.on('click' ,function(){
+        $modal.detach();
+        $('#login').addClass('hidden');
     });
-    /*$(document).on('click', $('.login') ,function(){
-        console.log('클릭');
+    $('.formToggle').ready(function(){
+        $('.formToggle').on('click', function(){
+            if($('.loginTextBox').hasClass('textBoxRight')){
+                $('.loginTextBox').removeClass('textBoxRight');
+                $('.loginTextBox').addClass('textBoxLeft');
+                $('.loginTextRight').addClass('hidden');
+                $('.loginTextLeft').removeClass('hidden');
+            }else {
+                $('.loginTextBox').removeClass('textBoxLeft');
+                $('.loginTextBox').addClass('textBoxRight');
+                $('.loginTextLeft').addClass('hidden');
+                $('.loginTextRight').removeClass('hidden');
+            }
+        });
     });
-    $(document).on('click', $('.search') ,function(){
-        console.log('클릭');
-    });*/
+    $('#footLang').ready(function(){
+        $('#footLang').on('click',function(){
+            $('.langBox').removeClass('lbHead hidden');
+            if($('.langBox').hasClass('lbFoot')){
+                $('.langBox').removeClass('lbFoot');
+                $('.langBox').addClass('hidden');
+            }else $('.langBox').addClass('lbFoot');
+        });
+    });
 });
