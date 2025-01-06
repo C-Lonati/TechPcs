857e76a5a1f8d37db49fba86f8e3a738480b8d34
C-Lonati <70873223+C-Lonati@users.noreply.github.com>
2025-01-06 16:49:34 +0900

    Add files via upload

---
+++ js/news.js
@@ -0,0 +1,33 @@
+$(function(){
+    if($(window).width()>1199)
+    $(window).on('scroll', function(){
+        if(window.scrollY>100)
+        $('#otherNews').css('top',100+window.scrollY + 'px');
+        else $('#otherNews').css('top','180px');
+    });
+    $('.category>li').on('click tap', function(){
+        $('.cateSelect').removeClass('cateSelect');
+        $(this).addClass('cateSelect');
+        $cate = $(this).text();
+        $('.cate').html($cate);
+        $('.hardwareNews .news').hide();
+        switch($cate){
+            case '전체보기' : $('.hardwareNews .news').show();
+            break;
+            case '업계동향' : $('.hardwareNews .news.trends').show();
+            break;
+            case 'CPU' : $('.hardwareNews .news.cpu').show();
+            break;
+            case 'GPU' : $('.hardwareNews .news.gpu').show();
+            break;
+            case '메인보드' : $('.hardwareNews .news.mainBoard').show();
+            break;
+            case 'PSU' : $('.hardwareNews .news.psu').show();
+            break;
+            case 'RAM' : $('.hardwareNews .news.ram').show();
+            break;
+            case '저장장치' : $('.hardwareNews .news.memory').show();
+            break;
+        }
+    });
+});
